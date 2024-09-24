76cd3897f723fe8ae331092c74b1bec112077a06
darek <dariusz16jedrzejek@gmail.com>
2024-09-24 15:25:39 +0100

    Moving global variables to a header

--- memory_mng/random_numbers.c
+++ memory_mng/random_numbers.c
@@ -1,18 +1,15 @@
 /* random_numbers.c     Generates 100 numbers from a range 1 - 10 in descending order
                         Sorts numbers using selection sort*/
-#include <stdio.h>
-#include <string.h>
+#include "random_numbers.h"
 
-static unsigned int seed =  7;
-#define NUMBERS             100
-
-int random();
+unsigned int random();
 void produce_random_numbers( int [] );
 void selection_sort( int [] );
 void print_numbers( int [] );
 
 int main( void )
 {   
+    extern unsigned int seed;
     int numbers[ NUMBERS ];
     produce_random_numbers( numbers );
     selection_sort( numbers );
@@ -21,11 +18,11 @@ int main( void )
     return 0;
 }
 
-int random( void )
+unsigned int random( void )
 {
     /* Generates random number*/
     seed = seed * 123456789 + 123454;
-    return ( int )( seed / 65536 ) % 10 + 1;
+    return ( seed / 65536 ) % 10 + 1;
 }
 
 void produce_random_numbers( int * array )
